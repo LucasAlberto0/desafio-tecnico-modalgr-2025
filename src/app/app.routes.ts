acd93b33cbab89cf5765a620c706ed612f163cca
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-02-02 03:02:17 -0300

    feat: home-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { DeveloperPageComponent } from './pages/developer-page/developer-page.component';
 
 export const routes: Routes = [
-    {   
-        path: '',
+    {
+        path: '', 
         component: HomeComponent
     },
+    {
+        path: 'register',
+        component: RegisterPageComponent
+    },
+    {
+        path: 'developer',
+        component: DeveloperPageComponent
+    }
 ];
