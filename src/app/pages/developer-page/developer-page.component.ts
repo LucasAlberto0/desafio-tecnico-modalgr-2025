df3b26ad8bacae9b2239b0c3627ae1c154a9df95
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-02-02 03:05:15 -0300

    feat: developer-component

---
+++ src/app/pages/developer-page/developer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-developer-page',
+  imports: [HeaderComponent],
+  templateUrl: './developer-page.component.html',
+  styleUrl: './developer-page.component.scss'
+})
+export class DeveloperPageComponent {
+
+}
